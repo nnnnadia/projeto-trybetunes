58a0536fd57c417631d06fb4bd53bf3b294b99df
nnnnadia <tristaonadia@gmail.com>
2022-05-22 16:27:49 -0300

    Requisito 2: É possível realizar o login.

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div>
+      <h3>Carregando...</h3>
+    </div>
+  );
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,83 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 export default class Login extends Component {
+  state = {
+    userInfo: {
+      name: '',
+    },
+    isLoginButtonDisabled: true,
+    loading: false,
+    isLogged: false,
+  };
+
+  verifyUserInfo = () => {
+    const { userInfo: { name } } = this.state;
+    const userNameMaxLength = 3;
+    if (name.length >= userNameMaxLength) {
+      this.setState({ isLoginButtonDisabled: false });
+    } else {
+      this.setState({ isLoginButtonDisabled: true });
+    }
+  }
+
+  handleLoginButton = async () => {
+    const { userInfo } = this.state;
+    this.setState({ loading: true });
+    await createUser(userInfo);
+    this.setState({ isLogged: true });
+  }
+
+  handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ userInfo: { [name]: value } },
+      () => this.verifyUserInfo());
+  }
+
   render() {
+    const {
+      state: {
+        userInfo: { name },
+        isLoginButtonDisabled,
+        loading,
+        isLogged,
+      },
+      handleInputChange,
+      handleLoginButton,
+    } = this;
+    if (isLogged) {
+      return <Redirect to="/search" />;
+    }
     return (
       <div data-testid="page-login">
-        <h2>Login</h2>
+        {
+          loading
+            ? (
+              <Loading />
+            )
+            : (
+              <>
+                <h2>Login</h2>
+                <input
+                  type="text"
+                  name="name"
+                  value={ name }
+                  onChange={ handleInputChange }
+                  data-testid="login-name-input"
+                />
+                <button
+                  type="button"
+                  onClick={ handleLoginButton }
+                  disabled={ isLoginButtonDisabled }
+                  data-testid="login-submit-button"
+                >
+                  Entrar
+                </button>
+              </>
+            )
+        }
       </div>
     );
   }
