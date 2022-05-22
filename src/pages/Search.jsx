df50041a200540f281ae6b251dadbcf8dc70723f
nnnnadia <tristaonadia@gmail.com>
2022-05-22 17:34:25 -0300

    Requisito 5: O campo e botão de busca são criados em Search.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,57 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    searchQueue: '',
+    isSearchButtonDisabled: true,
+  };
+
+  handleSearchButton = () => {
+
+  }
+
+  verifySearchQueue = () => {
+    const { searchQueue } = this.state;
+    if (searchQueue.length > 1) {
+      this.setState({ isSearchButtonDisabled: false });
+    } else {
+      this.setState({ isSearchButtonDisabled: true });
+    }
+  }
+
+  handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value },
+      () => this.verifySearchQueue());
+  }
+
   render() {
+    const {
+      state: {
+        searchQueue,
+        isSearchButtonDisabled,
+      },
+      handleInputChange,
+      handleSearchButton,
+    } = this;
     return (
       <div data-testid="page-search">
         <Header />
-        <h2>Search</h2>
+        <input
+          type="text"
+          name="searchQueue"
+          value={ searchQueue }
+          onChange={ handleInputChange }
+          data-testid="search-artist-input"
+        />
+        <button
+          type="button"
+          onClick={ handleSearchButton }
+          disabled={ isSearchButtonDisabled }
+          data-testid="search-artist-button"
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
