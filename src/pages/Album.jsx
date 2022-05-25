36677c36217e657298024de9f1c9b662099c851e
nnnnadia <tristaonadia@gmail.com>
2022-05-25 16:12:34 -0300

    Requisito 7: A página do álbum lista as músicas e disponibiliza um player com o preview.

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
 export default class Album extends Component {
+  state = {
+    loading: true,
+    albumInfo: [],
+  };
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const albumInfo = await getMusics(id);
+    this.setState({
+      loading: false,
+      albumInfo: [...albumInfo],
+    });
+  }
+
   render() {
+    const { loading, albumInfo } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        <h2>Album</h2>
+        { loading
+          ? <Loading />
+          : (
+            <div>
+              <h2 data-testid="album-name">{albumInfo[0].collectionName}</h2>
+              <h3 data-testid="artist-name">{albumInfo[0].artistName}</h3>
+              {albumInfo.filter((track) => track.trackId)
+                .map((track) => (
+                  <MusicCard
+                    key={ track.trackId }
+                    trackName={ track.trackName }
+                    previewUrl={ track.previewUrl }
+                  />))}
+            </div>
+          )}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
