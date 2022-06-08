37069b692f611fb441e1633f46173d8d226950d0
nnnnadia <tristaonadia@gmail.com>
2022-06-08 15:05:49 -0300

    Ao abrir a página de perfil a API é chamada e os dados são salvos no estado.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends Component {
+  state = {
+    loading: true,
+    userInfo: {},
+  };
+
+  async componentDidMount() {
+    const userInfo = await getUser();
+    this.setState({
+      loading: false,
+      userInfo,
+    });
+  }
+
   render() {
+    const { loading, userInfo } = this.state;
     return (
       <div data-testid="page-profile">
         <Header tab="profile" />
-        <h2>Profile</h2>
+        { loading
+          ? <h3 className="center-text"><Loading /></h3>
+          : null }
       </div>
     );
   }
